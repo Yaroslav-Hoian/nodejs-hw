607144399945f72edbee0c912593f0c9ae686536
Yaroslav Hoian <sgoyan1405@gmail.com>
2025-11-03 16:07:09 +0200

    create models,routes and controllers

---
+++ src/models/note.js
@@ -0,0 +1,36 @@
+import { model, Schema } from 'mongoose';
+
+const notesShema = new Schema({
+  title: {
+    type: String,
+    required: true,
+    trim: true,
+  },
+  content: {
+    type: String,
+    required: false,
+    trim: true,
+    default: '',
+  },
+  tag: {
+    type: String,
+    required: false,
+    enum: [
+      'Work',
+      'Personal',
+      'Meeting',
+      'Shopping',
+      'Ideas',
+      'Travel',
+      'Finance',
+      'Health',
+      'Important',
+      'Todo',
+    ],
+    default: 'Todo',
+  },
+
+},{timestamps: true,
+  versionKey: false,});
+
+export const Notes = model('notes', notesShema);
