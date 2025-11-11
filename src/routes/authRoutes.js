2f38f841091fe9194414207a614af5868ebc7215
Yaroslav Hoian <sgoyan1405@gmail.com>
2025-11-11 16:51:22 +0200

    add refresh session

--- src/routes/authRoutes.js
+++ src/routes/authRoutes.js
@@ -4,10 +4,17 @@ import {
   loginUserSchema,
   registerUserSchema,
 } from '../validations/authValisation.js';
-import { loginUser, registerUser } from '../controllers/authController.js';
+import {
+  loginUser,
+  logoutUser,
+  refreshUserSession,
+  registerUser,
+} from '../controllers/authController.js';
 
 const router = Router();
 
 router.post('/auth/register', celebrate(registerUserSchema), registerUser);
 router.post('/auth/login', celebrate(loginUserSchema), loginUser);
+router.post('auth/logout', logoutUser);
+router.post('auth/refresh', refreshUserSession);
 export default router;
