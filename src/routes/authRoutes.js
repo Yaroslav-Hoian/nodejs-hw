a8b2f560d2fff39a1ab858edc3933463fc3d1f69
Yaroslav Hoian <sgoyan1405@gmail.com>
2025-11-13 19:42:32 +0200

    fix router auth

--- src/routes/authRoutes.js
+++ src/routes/authRoutes.js
@@ -15,6 +15,6 @@ const router = Router();
 
 router.post('/auth/register', celebrate(registerUserSchema), registerUser);
 router.post('/auth/login', celebrate(loginUserSchema), loginUser);
-router.post('auth/logout', logoutUser);
-router.post('auth/refresh', refreshUserSession);
+router.post('/auth/logout', logoutUser);
+router.post('/auth/refresh', refreshUserSession);
 export default router;
